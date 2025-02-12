30df155fb95ae678754cdcc32a5a2b013433e4c8
Shannon Hennessey <s.hennessey13@gmail.com>
2025-02-12 10:11:29 -0900

    calculate tanner core area (50th percentile stations) and add outputs

---
+++ scripts/get_core_area.R
@@ -0,0 +1,64 @@
+## Purpose: To define tanner crab core area as stations with 50th percentile of CPUE
+##
+## NOTES:
+## - include corner stations in the quantile estimation or not??
+##   there might be some spatial considerations, because not on even grid...
+
+
+
+## Load packages
+library(crabpack)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+
+
+## Pull Tanner specimen data
+tanner <- get_specimen_data(species = "TANNER",
+                            region = "EBS")
+
+## Calculate station-level CPUE
+cpue <- crabpack::calc_cpue(crab_data = tanner,
+                            species = "TANNER",
+                            years = c(1988:2024))
+
+
+# Read in station general definitions, grid cell centroid
+stations <- read.csv("Y:/KOD_Survey/EBS Shelf/Data_Processing/Data/lookup_tables/station_lookup.csv")
+
+# Define corner stations
+corners <- stations %>% 
+           filter(STATION_TYPE == "MTCA_CORNER") %>%
+           pull(STATION_ID)
+ 
+
+# Identify stations in 50-100 CPUE percentile range
+cpue50_core <- cpue %>%
+               # filter(!STATION_ID %in% corners) %>% # remove corner stations
+               group_by(STATION_ID) %>%
+               summarise(MEAN_CPUE = mean(CPUE)) %>%
+               mutate(QUANTILE = ifelse(MEAN_CPUE > quantile(MEAN_CPUE, 0.50), 1, 0)) %>%
+               # filter(MEAN_CPUE > quantile(MEAN_CPUE, 0.50)) %>% # 187 stations, 174 excluding corners
+               ungroup() %>%
+               left_join(., stations %>% select(STATION_ID, LATITUDE, LONGITUDE))
+
+
+## Plot for visual check
+ggplot(ne_countries(scale = "medium", returnclass = "sf")) +
+  geom_sf() +
+  geom_point(data = cpue50_core, aes(x = LONGITUDE, y = LATITUDE, 
+                                     size = MEAN_CPUE/1000, color = as.factor(QUANTILE))) +
+  coord_sf(xlim = c(-180, -157), ylim = c(54, 62.5), expand = FALSE) +
+  theme_bw() +
+  theme(legend.position = "none")
+ggsave("./figures/tanner_core_area.png", height = 6, width = 10)
+
+
+## Write .csv for stations in 50th percentile of avg CPUE  
+write.csv(cpue50_core %>% 
+            filter(MEAN_CPUE > quantile(MEAN_CPUE, 0.50)) %>%
+            select(-QUANTILE), 
+          "./outputs/tanner_area_cpue50.csv", row.names = FALSE)
+
+
+
