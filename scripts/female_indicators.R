0e388aa833bb417fbd61e568f07f6834e4a80c11
Shannon Hennessey <s.hennessey13@gmail.com>
2025-03-25 14:54:07 -0800

    add female mature size and clutch fullness indicators

---
+++ scripts/female_indicators.R
@@ -0,0 +1,109 @@
+## Purpose: To calculate (1) mean female Tanner Crab size at maturity and 
+##          (2) the proportion of primiparous mature female Tanner Crab 
+##          with full clutches.
+##
+## Author: Shannon Hennessey
+##
+## NOTES:
+## - look at differences in E166 vs. W166?
+## - mature size -- new hardshell females only? yes?
+## - clutch fullness -- how do we make this relate to fecundity more??
+##   - lg half full female might still be more fecund than small full female
+##   - fit a mroel that accounts for size?
+## - sperm limitation? i.e. why are we seeing low clutch fullness?
+
+
+## Read in setup
+source("./scripts/setup.R")
+
+
+## Calculate mean mature female size (new hardshell only)
+female_size <- calc_bioabund(crab_data = tanner,
+                             species = "TANNER",
+                             region = "EBS",
+                             year = years,                             
+                             crab_category = "mature_female",
+                             shell_condition = "new_hardshell",
+                             bin_1mm = TRUE)
+
+mean_size <- female_size %>%
+             group_by(YEAR) %>%
+             summarize(MEAN_SIZE = weighted.mean(SIZE_1MM, w = ABUNDANCE)) %>%
+             right_join(., expand.grid(YEAR = years)) %>%
+             arrange(YEAR)
+             
+
+
+# Plot
+ggplot(mean_size, aes(x = YEAR, y = MEAN_SIZE)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Year", y = "Mean Mature Female Tanner Crab Size (mm)") +
+  geom_hline(aes(yintercept = mean(MEAN_SIZE, na.rm = TRUE)), linetype = 5) +
+  xlim(min(years), max(years)) +
+  theme_bw()
+ggsave("./figures/female_SAM.png")
+
+
+# Save output
+write_csv(mean_size, "./outputs/female_SAM.csv")
+
+
+
+## Calculate proportion of primiparous mature female Tanner Crab with full clutches
+# calculate abundance of all primiparous mature females
+primip <- tanner
+primip$specimen <- primip$specimen %>%
+                   filter(SEX == 2,
+                          CLUTCH_SIZE > 0,
+                          SHELL_CONDITION %in% c(0:2))
+
+primip_abund <- calc_bioabund(crab_data = primip,
+                              species = "TANNER",
+                              region = "EBS",
+                              year = years,
+                              spatial_level = "region") %>%
+                mutate(PRIMIP = ABUNDANCE) %>%
+                select(YEAR, PRIMIP)
+
+
+# calculate abundance of just primiparous mature females with full clutches
+primip_full <- tanner
+primip_full$specimen <- primip_full$specimen %>%
+                        filter(SEX == 2,
+                               CLUTCH_SIZE > 0,
+                               SHELL_CONDITION %in% c(0:2),
+                               CLUTCH_SIZE %in% c(5:6))
+
+primip_full_abund <- calc_bioabund(crab_data = primip_full,
+                                   species = "TANNER",
+                                   region = "EBS",
+                                   year = years,
+                                   spatial_level = "region") %>%
+                     mutate(FULL_PRIMIP = ABUNDANCE) %>%
+                     select(YEAR, FULL_PRIMIP)
+
+# calculate proportion
+clutch_dat <- full_join(primip_full_abund, primip_abund) %>%
+              mutate(PROP_FULL = FULL_PRIMIP/PRIMIP) %>%
+              right_join(., expand.grid(YEAR = years)) %>%
+              arrange(YEAR)
+               
+
+
+# Plot
+ggplot(clutch_dat, aes(x = YEAR, y = PROP_FULL)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Year", y = "Proportion Primiparous Females with Full Clutch") +
+  geom_hline(aes(yintercept = mean(PROP_FULL, na.rm = TRUE)), linetype = 5) +
+  xlim(min(years), max(years)) +
+  theme_bw()
+ggsave("./figures/clutch_fullness.png")
+
+
+# Save output
+clutch_dat %>%
+  select(YEAR, PROP_FULL) %>%
+  write_csv("./outputs/clutch_fullness.csv")
+
