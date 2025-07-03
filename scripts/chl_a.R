70f5a9b8c58f126cd07bedf34f24650eaa732201
Shannon Hennessey <s.hennessey13@gmail.com>
2025-07-03 11:04:55 -0800

    change .nc file directory

--- scripts/chl_a.R
+++ scripts/chl_a.R
@@ -23,7 +23,7 @@ ext <- bsierp %>%
 
 # ## Download data
 # for(i in years){
-#   file_name <- paste0(data_dir, "nc/occ8_", i ,".nc")
+#   file_name <- paste0(data_dir, "OCC_nc/occ8_", i ,".nc")
 #   download.file(url = paste0("https://coastwatch.pfeg.noaa.gov/erddap/griddap/pmlEsaCCI60OceanColor8Day.nc?chlor_a%5B(",
 #                              i,"-04-01T00:00:00Z):(", i,"-07-31T00:00:00Z)%5D%5B(",ext[2],"):(",ext[4],")%5D%5B(",ext[1],
 #                              "):(",ext[3],")%5D&.draw=surface&.vars=longitude%7Clatitude%7Cchlor_a&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff"),
@@ -48,7 +48,7 @@ tidy_chl <- function(file) {
 datalist = list()
 
 for(i in 1998:max(years)){
-  dat <- tidy_chl(paste0(data_dir, "nc/occ8_", i ,".nc"))
+  dat <- tidy_chl(paste0(data_dir, "OCC_nc/occ8_", i ,".nc"))
   datalist[[i]] <- dat 
 }
 
